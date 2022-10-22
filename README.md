29617e9712b764060c5c077bc79241c525cefc23
Michel Maia <programador.michel.maia@gmail.com>
2022-10-22 01:36:58 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Pokedex
- desenvolvido para aprendizado
+
+![preview](./.github/preview.png)
+
+
+Desenvolvido para aprendizado. Realizado Pokedex com JavaScript. 22/10/22
+
+
+## 💻 Tecnologias
+
+- HTML
+- CSS
+- Javascript
+
+
+## Referência
+
+- [Youtube] https://www.youtube.com/c/RogerMelo
+
---
+++ app.js
@@ -0,0 +1,35 @@
+const getPokemonUrl =  id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const fethPokemon = () => {
+
+    const pokemonPromises = []
+
+    for (let i = 1; i <= 150; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+    } 
+
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            console.log(pokemons)
+
+            const listPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+                accumulator += `
+                <li class="card ${types[0]}">
+                <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
+                <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                <p class="card-subtitle"> ${types.join(' | ')}</p>
+                </li>
+                `
+                return accumulator
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"]')
+
+            ul.innerHTML = listPokemons
+        })
+
+}
+
+fethPokemon()
